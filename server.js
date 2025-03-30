4046d1badaaa7b7eef3cf41df20ee26e9b424d74
Tirumalasetty Harika <abhi8341508426@gmail.com>
2025-03-30 07:26:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,4 +12,5 @@ app.use(express.static("public")); // Serve static HTML files
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
+const DB_NAME = process.env.DB_NAME || 'default.sqlite';
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
